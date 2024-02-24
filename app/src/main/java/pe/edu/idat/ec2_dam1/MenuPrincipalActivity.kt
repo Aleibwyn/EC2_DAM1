6b26baf562e4f88ea068fdd14292bd776244a53f
Aleibwyn <mf-a@outlook.com>
2024-02-24 12:49:26 -0500

    Menú principal finalizado

---
+++ app/src/main/java/pe/edu/idat/ec2_dam1/MenuPrincipalActivity.kt
@@ -0,0 +1,11 @@
+package pe.edu.idat.ec2_dam1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuPrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_principal)
+    }
+}
