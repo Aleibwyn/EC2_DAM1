e8f7552e363eeb37190e34983721bf96638b9a11
Aleibwyn <mf-a@outlook.com>
2024-02-24 16:21:07 -0500

    Formulario Cuestionario - Diseño Inical - Finalizado

---
+++ app/src/main/java/pe/edu/idat/ec2_dam1/FormularioCuestionarioActivity.kt
@@ -0,0 +1,11 @@
+package pe.edu.idat.ec2_dam1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormularioCuestionarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_cuestionario)
+    }
+}
