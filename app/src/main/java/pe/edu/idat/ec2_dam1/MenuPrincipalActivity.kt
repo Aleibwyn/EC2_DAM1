b90428db092c4974c2416c36dc671b79c5ef3ac2
Roxana <a19103729@idat.edu.pe>
2024-02-24 18:48:46 -0500

    Implementando funcionalidad en los botones del menu principal

--- app/src/main/java/pe/edu/idat/ec2_dam1/MenuPrincipalActivity.kt
+++ app/src/main/java/pe/edu/idat/ec2_dam1/MenuPrincipalActivity.kt
@@ -1,11 +1,32 @@
 package pe.edu.idat.ec2_dam1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pe.edu.idat.ec2_dam1.databinding.ActivityMenuPrincipalBinding
 
 class MenuPrincipalActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMenuPrincipalBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu_principal)
+        binding = ActivityMenuPrincipalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegistro.setOnClickListener {
+            val opRegistro = Intent(this, RegistroActivity::class.java)
+            startActivity(opRegistro)
+        }
+
+        binding.btnFormulario.setOnClickListener {
+            val opFormulario = Intent(this, FormularioCuestionarioActivity::class.java)
+            startActivity(opFormulario)
+        }
+
+        binding.btnListado.setOnClickListener {
+            val opListado = Intent(this, ListadoActivity::class.java)
+            startActivity(opListado)
+        }
     }
 }
