98cbce4dfec96b104f63a950e645f9ac19a19562
Garou1207 <kevinbarrutia@outlook.com>
2024-02-25 09:15:15 -0500

    Cambios en Menu Principal / onCLick btnFormulario

--- app/src/main/java/pe/edu/idat/ec2_dam1/MenuPrincipalActivity.kt
+++ app/src/main/java/pe/edu/idat/ec2_dam1/MenuPrincipalActivity.kt
@@ -1,11 +1,26 @@
 package pe.edu.idat.ec2_dam1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import pe.edu.idat.ec2_dam1.databinding.ActivityMenuPrincipalBinding
 
-class MenuPrincipalActivity : AppCompatActivity() {
+class MenuPrincipalActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMenuPrincipalBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu_principal)
+        binding = ActivityMenuPrincipalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnFormulario.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            R.id.btnFormulario -> startActivity(Intent(applicationContext,
+                    FormularioCuestionarioActivity::class.java))
+                }
     }
 }
