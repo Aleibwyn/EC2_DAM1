05924359be652f0f1924d4311ef3c7e90981117b
Roxana <a19103729@idat.edu.pe>
2024-02-26 00:44:58 -0500

    Actualizando diseño del menu principal y el formulario de registro

--- app/src/main/java/pe/edu/idat/ec2_dam1/ListadoRegistroActivity.kt
+++ app/src/main/java/pe/edu/idat/ec2_dam1/ListadoRegistroActivity.kt
@@ -2,10 +2,16 @@ package pe.edu.idat.ec2_dam1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import pe.edu.idat.ec2_dam1.databinding.ActivityListadoRegistroBinding
 
 class ListadoRegistroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityListadoRegistroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_listado_registro)
+        binding = ActivityListadoRegistroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/pe/edu/idat/ec2_dam1/RegistroActivity.kt
+++ app/src/main/java/pe/edu/idat/ec2_dam1/RegistroActivity.kt
@@ -1,5 +1,6 @@
 package pe.edu.idat.ec2_dam1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -8,6 +9,7 @@ import pe.edu.idat.ec2_dam1.databinding.ActivityRegistroBinding
 class RegistroActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegistroBinding
+    private val datosRegistrados: MutableList<String> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,9 +38,12 @@ class RegistroActivity : AppCompatActivity() {
         binding.btnAcceder.setOnClickListener {
             if (validarCampos()) {
                 Toast.makeText(this, "Datos validados correctamente", Toast.LENGTH_SHORT).show()
+                val opListadoRegistro = Intent(this, ListadoRegistroActivity::class.java)
+                startActivity(opListadoRegistro)
             }
         }
     }
+
     private fun validarCampos(): Boolean {
         val dni = binding.etDni.text.toString().trim()
         val nombre = binding.etNombre.text.toString().trim()
@@ -76,7 +81,6 @@ class RegistroActivity : AppCompatActivity() {
             }
 
         }
-
         return true
     }
 
